8a0aa144588dd32d53dbc594b7d97b7d729c52f1
Ryan <mryan23@vt.edu>
2013-06-27 23:11:39 -0700

    adding action to music.js

--- scripts/music.js
+++ scripts/music.js
@@ -9,4 +9,29 @@ port.onMessage.addListener(function(msg) {
     console.log('madame who?');
     port.postMessage({answer: "Madame... Bovary"});
   }
+
+  if(msg.action !== undefined){
+  	var button;
+  	switch(msg.action){
+  		case "play":
+  		case "pause":
+  			button=$("button[data-id='play-pause']");
+  			break;
+  		case "repeat":
+  			button=$("button[data-id='repeat']");
+  			break;
+  		case "shuffle":
+  			button=$("button[data-id='shuffle']");
+  			break;
+  	}
+  	triggerMouseEvent(button,'click');
+
+  }
+
 });
+
+function triggerMouseEvent(element, eventname){
+    var event = document.createEvent('MouseEvents');
+    event.initMouseEvent(eventname, true, true, document.defaultView, 1, 0, 0, 0, 0, false, false, false, false, 0, element);
+    element.dispatchEvent(event);
+}
